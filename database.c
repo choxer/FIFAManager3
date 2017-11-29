dc0497aee9d6a32bbeefaecad5aa5b9944b04dd0
choxer <33546322+choxer@users.noreply.github.com>
2017-11-29 19:33:12 +0100

    Add files via upload

--- database.c
+++ database.c
@@ -1,35 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void load()
 {
-    //char Zeile[101];
 
-    FILE *Datei;
-    Datei = fopen("test.txt", "r");
-
-    char Array[200];
-
-    if(Datei)
-    {
-        fscanf(Datei, "%s", &Array[0] );
-        printf("%s\n", Array);
-
-        fclose(Datei);
-    }
-
-    // Leerzeichen und TABS überspringen:
-    char Zeile[101];
-    char *Zeilenanfang;
-    Zeilenanfang = Zeile;   // zeiger zeigt nun auf Zeilenanfang
-
-    while ( (*Zeilenanfang == ' ')||(*Zeilenanfang == 9) )      // leerzeichen o. ASCII 9 nicht beachten
-    {
-        Zeilenanfang++;
-    }
-    // erkennen von ' < '
-    if (*Zeilenanfang == '<')
-        printf("< entdeckt!");
-    // welcher Tag ?
-    if ( strncmp( Zeile, "<Title>", 7) == 0 ) // bei 0 -> GLEICH!
 }
