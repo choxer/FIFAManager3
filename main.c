b7b221064c681c54c16b87d81d9a257932048b8e
choxer <33546322+choxer@users.noreply.github.com>
2017-11-29 19:33:46 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,100 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "datastruct.h"
-#include "Team.h"
-#include "tools.h"
-#include "menu.h"
-#include "datetime.h"
-#include "database.h"
-#define NULL 0
-//------------Mannschaften-Verwaltung-----------------------//
-
-
-//Angaben zu Menutitel, Anzahl der Untermenus und derer Namen //
-
 
 int main()
 {
 
+    char Zeile[101];
+    char Zeile2[101];
+    char Zeile3[101];
+    char Zeile4[101];
 
-    int anzmenu=8;                          //ANzahl der menus
+    char *Zeilenanfang;
+    Zeilenanfang = Zeile;
 
-    TMTitel ArrMTitel[anzmenu];         //Array von struct TMenuTitel
-                                    //in struct char MTitel[]
+    FILE *Datei = fopen("test.txt", "r");
+    if(Datei)
+    {
 
-     //füllen der Titel
-     strcpy(ArrMTitel[0].MTitel,"Neue Mannschaft anlegen");
-     strcpy(ArrMTitel[1].MTitel,"Spieler hinzufuegen");
-     strcpy(ArrMTitel[2].MTitel,"Spieler loeschen");
-     strcpy(ArrMTitel[3].MTitel,"Mannschaft loeschen");
-     strcpy(ArrMTitel[4].MTitel,"Suchen");
-     strcpy(ArrMTitel[5].MTitel,"Sortieren");
-     strcpy(ArrMTitel[6].MTitel,"Auflisten");
-     strcpy(ArrMTitel[7].MTitel,"Programm beenden");
+        fscanf(Datei, "%s[^\n]", Zeile2);
+        fscanf(Datei, "%s[^\n]", Zeile);
+        fscanf(Datei, "%s[^\n]", Zeile3);
+        //fscanf(Datei, "%s[^\n]", Zeile4);
+        fgets(Zeile4, 100, Datei);
 
+        printf("%s\n", Zeile);
+        printf("%s\n", Zeile2);
+        printf("%s\n", Zeile3);
+        printf("%s\n", Zeile4);
 
-    char Menutitel[]={"Mannschaften-Verwaltung"};
-
-
-    //-----------------Aufruf von Menu funktion--------------------------------//
-
-    int ok=0;
-    do{
-        int eingabe = getmenu((&Menutitel[0]),ArrMTitel,anzmenu);
-
-        switch(eingabe) {
-            case 0:
-
-                    createTeam();
-                    WaitForEnter(); break;
-
-            case 1:
-
-                    do
-                    {
-
-
-                    TPlayer *newPlayer;
-                    newPlayer = malloc(sizeof(TPlayer));
-
-                    if ( newPlayer == NULL )
-                    {
-                        printf("ERROR:SPEICHERRESERVIERUNG NEWPLAYER");
-                    }else
-                    {
-                        addPlayer();
-                    }
-
-                    free(newPlayer);
-                    }while( askYesorNo("Wollen sie noch einen Spieler anlegen?") );
-
-
-
-                    break;
-
-            case 2: deletePlayer();
-                    WaitForEnter(); break;
-
-            case 3: deleteTeam();
-                    WaitForEnter(); break;
-
-            case 4: searchPlayer();
-                    WaitForEnter(); break;
-
-            case 5: sortTeams();
-                    WaitForEnter(); break;
-
-            case 6: listTeams();
-                    WaitForEnter(); break;
-
-            case 7: endprog();ok=1;   break;
-
-            //case 8: load(); break;
-
-            default:printf("ERROR:Menuauswahl\n") ; break;
-            }
-    }while(!ok);
-    load();
+    }
     return 0;
 }
